cc1a01d8affb78f5eabb8d5aa5f2e69c0b3682a2
a-gbatie <ange.batie@gmail.com>
2020-12-14 10:21:21 -0800

    commit tree 59f02ea6667804a6195ef4a173058bb5911c200a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :update]
   
   def index
     @students = Student.all
   end
 
   def show
+    # @student = Student.find(params[:id])
+  end
+
+
+  def update
+    # @student = Student.find(params[:id]) => not needed beacuse set_student is helper metheod. Update has access to @student variable.
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
   end
 
   private
